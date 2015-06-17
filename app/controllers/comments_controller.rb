c40e9cfdbb78d22f35e30334a4e5835d325bcb96
Ritima Ruby <sharma.ritima@gmail.com>
2015-06-17 12:58:12 +0530

    now it allows creating comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = article.comments.create(comment_params)
+    redirect_to @comment.article
+  end
+  
+  private
+  def article
+    Article.find(params['article_id'])
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:body, :user_id)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617064231) do
+ActiveRecord::Schema.define(version: 20150617065501) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "article_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
